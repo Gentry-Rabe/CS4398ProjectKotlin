318558276314714aa864528538c66f4444e7f042
Gin <gentryrabe@gmail.com>
2025-12-06 15:45:26 -0600

    Adjustment for merge conflict

---
+++ app/laundryapp/model/UserAccount.java
@@ -0,0 +1,96 @@
+package app.model;
+
+import java.util.List;
+
+public class UserAccount {
+    private int userId;
+    private String name;
+    private String email;
+    private String passwordHash;
+    private String location;
+    private PaymentInfo paymentInfo;
+    private List<Order> orders;
+
+    public UserAccount(int userId, String name, String email, String passwordHash, String location,
+    PaymentInfo paymentInfo, List<Order> orders) {
+        this.userId = userId;
+        this.name = name;
+        this.email = email;
+        this.passwordHash = passwordHash;
+        this.location = location;
+        this.paymentInfo = paymentInfo;
+        this.orders = orders;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public PaymentInfo getPaymentInfo() {
+        return paymentInfo;
+    }
+
+    public void setPaymentInfo(PaymentInfo paymentInfo) {
+        this.paymentInfo = paymentInfo;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(List<Order> orders) {
+        this.orders = orders;
+    }
+
+    public void addOrder(Order order) {
+        this.orders.add(order);
+    }
+
+    @Override
+    public String toString() {
+        return "UserAccount{" +
+                "userId=" + userId +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", location='" + location + '\'' +
+                ", paymentInfo=" + paymentInfo +
+                ", ordersCount=" + (orders != null ? orders.size() : 0) +
+        '}';
+    }
+}
